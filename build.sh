fe320452bf3ab789d6f656d5de9071a93f9bef04
Ignacio Sanchez Gines <nacho@geardome.com>
2020-04-14 17:57:39 +0200

    Update protos

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 GOOGLE_APIS=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis
 
-for proto in user product order
+for proto in user product order account
 do
   echo "Removing ${proto}/*.go"
   rm -f ${proto}/*.go
